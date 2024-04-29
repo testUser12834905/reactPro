0ce9e70522ff011010c77f2d1f1fdd122fe9d429
phoenix21212 <80974944+phoenix21212@users.noreply.github.com>
2024-04-29 18:51:06 +0200

    add styled component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,32 @@
-import React, { useEffect, useState } from "react"
-import getQuotes, { QuoteType } from "../api/get-quotes"
+import React, { useEffect, useState } from "react";
+import getQuotes, { QuoteType } from "../api/get-quotes";
+import styled from "styled-components";
+
+const QuoteParag = styled.div`
+	padding: 20px;
+	margin: 40px auto;
+	max-width: 800px;
+	background-color: gray;
+	border-left: 5px solid #3498db;
+	color: white;
+	font-size: 24px;
+	text-align: center;
+	box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+`;
 
 const Home = () => {
-	const [quote, setQuote] = useState<QuoteType>()
+	const [quote, setQuote] = useState<QuoteType>();
 
 	useEffect(() => {
-		getQuotes().then((res) => setQuote(res))
-	}, [])
+		getQuotes().then((res) => setQuote(res));
+	}, []);
 
 	return (
 		<>
 			<div>Home</div>
-			<p>{quote?.content}</p>
+			<QuoteParag>{quote?.content}</QuoteParag>
 		</>
-	)
-}
+	);
+};
 
-export default Home
+export default Home;
